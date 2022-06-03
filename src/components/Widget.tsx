02f8c15acd6ebb4ef196d8566a5bc2a926ad6b7e
Ryan Costa <ryan.costa122@gmail.com>
2022-06-03 14:27:21 -0300

    first-commit

---
+++ src/components/Widget.tsx
@@ -0,0 +1,9 @@
+import { ChatTeardropDots } from 'phosphor-react';
+
+export function Widget(){
+  return (
+    <button>
+      <ChatTeardropDots className="bg-violet-500 w-6 h-6" rounded-full/>
+    </button>
+  )
+}
